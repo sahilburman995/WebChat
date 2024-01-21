80a79ce3ff2bc4b2e77ca3135c7f63bfb97ad6f6
sahil burman <sahiburman277@gmail.com>
2024-01-21 19:23:41 +0530

    styleadded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import {io }from "socket.io-client";
+import './App.css';
 
 const App = () => {
  
   const socket =useMemo(()=>io("http://localhost:3000"),[]);
 
+  const[socketid,setsocketid]=useState('');
   
-  
-  
-
+  const [receivedMessages, setReceivedMessages] = useState([]);
   
     const [message, setMessage] = useState('');
   
@@ -24,7 +24,7 @@ const App = () => {
     useEffect(()=>{
 
       socket.on("connect",()=>{
-  
+   setsocketid(socket.id);
         console.log("connected",socket.id);
       })
   
@@ -33,6 +33,7 @@ const App = () => {
       })
       socket.on("recive",(a)=>{
         console.log(a);
+        setReceivedMessages((prevMessages) => [...prevMessages, a]);
       })
   
      
@@ -46,15 +47,33 @@ const App = () => {
 
   return (
     <>
-    <form onSubmit={handleSubmit} className="send-message-form">
-    <input
-      type="text"
-      placeholder="Type your message..."
-      value={message}
-      onChange={(e) => setMessage(e.target.value)}
-    />
-    <button type="submit">Send</button>
-  </form>
+    {/* Navigation Bar */}
+    <div className="navbar">
+      <span>WEBCHAT</span>
+      
+    </div>
+    <div className='myid' style={{alignItems:'center'}}> <div className='myids'><h2>Room ID </h2>  {socketid}</div></div>
+    
+    {/* Chat Interface */}
+    <div className="chat-interface">
+      {/* Message Input Form */}
+      <form onSubmit={handleSubmit} className="send-message-form">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+
+      {/* Display the received messages in a div with message-bubble style */}
+      <div className="received-messages">
+        {receivedMessages.map((msg, index) => (
+          <div key={index} className="message-bubble">{msg}</div>
+        ))}
+      </div>
+    </div>
   </>
   )
 }
